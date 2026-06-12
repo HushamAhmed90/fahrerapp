124868d6f4be51e9cd5888b77e48555e844a3dcb
HushamAhmed90 <earthlinkhaweja@gmail.com>
2026-06-12 21:32:53 +0200

    Fix import path for firebase in page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from "react";
 import { collection, getDocs, doc, updateDoc } from "firebase/firestore";
-import { db } from "../../firebase";
+import { db } from "../firebase";
 
 type Stop = {
   id: string;
